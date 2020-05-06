94057aaa8549b3d59cdd5ef544618b60bc473d1c
toniktoo <toniktoo@mail.ru>
2020-05-06 06:53:33 +0300

    Updates

---
+++ precache-manifest.41d1ef1785374b3e564818e13cecfbe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d4bdec9c9fa1b6fb07fa82a96dcf1d",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "9e8bc6c76e21b99b030a",
+    "url": "/aviasales/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "981e8087d9bfe6e30098",
+    "url": "/aviasales/static/css/main.c31fd722.chunk.css"
+  },
+  {
+    "revision": "9e8bc6c76e21b99b030a",
+    "url": "/aviasales/static/js/2.e961b823.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/aviasales/static/js/2.e961b823.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981e8087d9bfe6e30098",
+    "url": "/aviasales/static/js/main.5ffb771f.chunk.js"
+  },
+  {
+    "revision": "d966f2b871a96fd638ae",
+    "url": "/aviasales/static/js/runtime-main.7eab63ba.js"
+  }
+]);
